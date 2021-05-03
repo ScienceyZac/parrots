65048273a44ef7448e44d7d869d13e5abb73b57f
Scienceyzac <scienceyzac@gmail.com>
2021-05-03 18:27:53 +1000

    added more info

--- src/app/parrots/parrots.component.ts
+++ src/app/parrots/parrots.component.ts
@@ -11,6 +11,11 @@ export class ParrotsComponent {
     this.getParrots();
   }
   addParrot = (parrot) => {
+    if (!parrot.name) return alert('you need to provide a name');
+    if (!parrot.img) return alert('you need to provide a image');
+    if (!parrot.desc) return alert('you need to provide a description');
+    if (this.parrots.filter((p) => p.name == parrot.name)[0])
+      return alert(`the parrot: "${parrot.name}" already exists`);
     localStorage.setItem(parrot.name, JSON.stringify(parrot));
     this.parrots.push(parrot);
     console.log(this.parrots);
@@ -23,5 +28,17 @@ export class ParrotsComponent {
       this.parrots.push(JSON.parse(parrot));
       console.log(this.parrots);
     }
+  };
+  delete = (name) => {
+    for (let i = 0; i < this.parrots.length; i++) {
+      if (this.parrots[i].name == name) {
+        this.parrots.splice(i, 1);
+      }
+    }
+    // this.parrots.filter((n) => n.name == name)[0] = null;
+    localStorage.removeItem(name);
+    console.log(this.parrots);
+    console.log(this.parrots.filter((n) => n.name == name));
+    console.log(this.parrots.filter((n) => n.name == name)[0]);
   };
 }
