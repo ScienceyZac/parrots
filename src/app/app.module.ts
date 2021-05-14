fe636ed41f7ae22c2b377be53cb23b8f3ee27c05
Scienceyzac <scienceyzac@gmail.com>
2021-05-14 12:26:46 +1000

    CHANGE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParrotsComponent } from './parrots/parrots.component';
 import { EditComponent } from './edit/edit.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ParrotsComponent,
-    EditComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ParrotsComponent, EditComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
